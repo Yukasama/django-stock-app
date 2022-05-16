ab9b0c5b9cea03c06750be87bde270f8010737db
Yukasama <yukasamaa@gmail.com>
2022-05-16 18:23:23 +0200

    Fixed Database Issue (Finally)

--- Aethega/settings.py
+++ Aethega/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import sqlite3 as lite
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -95,7 +96,11 @@ WSGI_APPLICATION = 'Aethega.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3.db'),
+        'HOST': 'localhost',
+        'PASSWORD': '',
+        'USER': '',
+        'PORT': '',
     }
 }
 
